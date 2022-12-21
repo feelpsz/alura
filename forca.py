e234a6d925c9577ff4e800dca7e23e64fba45976
Felipe Galante Simões <fgalantesimoes@gmail.com>
2022-12-21 10:10:27 -0300

    Adição do arquivo de Forca

---
+++ forca.py
@@ -0,0 +1,20 @@
+
+def jogar():
+
+
+    print("******************************************************************")
+    print("*******************Bem-vindo ao jogo de forca!!*******************")
+    print("*******Você terá 5 chances para advinhar a Palavra Secreta!*******")
+    print("******************************************************************")
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("Jogando...")
+        print(palavra_secreta)
+        break
+
+if(__name__ == "__main__"):
+    jogar()
