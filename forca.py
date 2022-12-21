1683b6ecea9acea2fdc5f63d09e526438edc5188
Felipe Galante Simões <fgalantesimoes@gmail.com>
2022-12-21 10:56:17 -0300

    Alterações até a aula 2.

--- forca.py
+++ forca.py
@@ -12,9 +12,19 @@ def jogar():
     acertou = False
 
     while(not enforcou and not acertou):
+
+        chute = input("Digite o seu chute: ")
+        chute = chute.strip()
+        index = 0
+        index = index + 1
+
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("A letra {} foi encontrada na posição {}".format(letra, index))
+            index = index + 1
+
         print("Jogando...")
-        print(palavra_secreta)
-        break
+
 
 if(__name__ == "__main__"):
     jogar()
